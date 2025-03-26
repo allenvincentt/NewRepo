f0ac4a21325f2cf4b9cf711a155db257f3c034db
CpE Laboratory <CpE Laboratory@BE216-PC12>
2025-03-26 14:46:07 +0800

    added Programs

---
+++ SMS.Domain/Programs.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Domain
+{
+    public class Programs
+    {
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public string? Description { get; set; }
+    }
+}
